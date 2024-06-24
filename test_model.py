142978e5783589a766b588c454eda4eb0ed858f4
arihant-jha <arihantj20@iitk.ac.in>
2024-06-24 08:16:28 +0530

    tests - 1

--- test_model.py
+++ test_model.py
@@ -8,25 +8,50 @@ tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
 
 
+def make_batch_and_order_line(
+    sku,
+    batch_qty,
+    line_qty
+):
+    return (
+        Batch("Batch-001", sku, batch_qty, eta=today), 
+        OrderLine("Order-1", sku, line_qty)
+    )
+
 def test_allocating_to_a_batch_reduces_the_available_quantity():
+    batch, line = make_batch_and_order_line("sofa", 30, 20)
+    batch.allocate(line)
+    assert batch.available_quantity == 10
     pytest.fail("todo")
 
 
 def test_can_allocate_if_available_greater_than_required():
+    batch, line = make_batch_and_order_line("sofa", 30, 20)
+    assert batch.can_allocate(line) == True
     pytest.fail("todo")
 
 
 def test_cannot_allocate_if_available_smaller_than_required():
+    batch, line = make_batch_and_order_line("sofa", 10, 20)
+    assert batch.can_allocate(line) == False
     pytest.fail("todo")
 
 
 def test_can_allocate_if_available_equal_to_required():
+    batch, line = make_batch_and_order_line("sofa", 20, 20)
+    assert batch.can_allocate(line) == True
     pytest.fail("todo")
 
 
 def test_prefers_warehouse_batches_to_shipments():
+    warehouse_batch = Batch("batch-w", "sofa", 20, eta=today)
+    shipment_batch = Batch("batch-s", "sofa", 20, eta=later)
+    line = OrderLine("line-1", "sofa", 8)
+    allocate(line, [warehouse_batch, shipment_batch])
+    assert warehouse.available_qty == 12 and shipment_batch.available_qty == 20
     pytest.fail("todo")
 
 
 def test_prefers_earlier_batches():
+    early = 
     pytest.fail("todo")
