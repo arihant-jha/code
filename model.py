cb9f24ccae112eaf568f86f2019faf16286dc3ff
arihant-jha <arihantj20@iitk.ac.in>
2024-06-25 17:35:57 +0530

    added _allocations

--- model.py
+++ model.py
@@ -1,25 +1,54 @@
 from dataclasses import dataclass
 from datetime import date, datetime
-from typing import Optional
+from typing import List, Optional
 
 
-@dataclass(frozen=True)#frozen=True makes it immutable
+@dataclass(frozen=True)  # frozen=True makes it immutable
 class OrderLine:
     orderid: str
     sku: str
     qty: int
+
 
 class Batch:
     def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_qty = qty
-        
-    def allocate(self, line: OrderLine):
-        self.available_qty -= line.qty
+        self._purchased_qty = qty
+        self._allocations = set()
     
+    @property
+    def allocated_qty(self) -> int:
+        return sum(line.qty for line in self._allocations)
+    
+    @property
+    def available_qty(self) -> int:
+        return self._purchased_qty - self.allocated_qty
+
+
     def can_allocate(self, line: OrderLine):
-        return (
-            self.available_qty > line.qty
-        )
+        return self.sku == line.sku and self.available_qty >= line.qty
+    
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+    
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+def allocate(line: OrderLine, batches: List[Batch]) -> None:
+    # allocate to the batch with smallest eta
+    if len(batches) == 0:
+        raise Exception("No Batches available for allocation")
+    best_batch = None
+    for i, batch in enumerate(batches):
+        if best_batch is None and batch.available_qty > line.qty:
+            best_batch = batch
+        if batch.eta < best_batch.eta and batch.available_qty > line.qty:
+            best_batch = batch
+    if best_batch is None:
+        raise Exception("No Batches available with requested quantity")
+    best_batch.allocate(line)
+    return
--- test_model.py
+++ test_model.py
@@ -1,7 +1,7 @@
 from datetime import date, timedelta
 import pytest
 
-from model import Batch, OrderLine
+from model import Batch, OrderLine, allocate
 
 # from model import ...
 
@@ -24,25 +24,21 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
     batch, line = make_batch_and_order_line("sofa", 30, 20)
     batch.allocate(line)
     assert batch.available_qty == 10
-    pytest.fail("todo")
 
 
 def test_can_allocate_if_available_greater_than_required():
     batch, line = make_batch_and_order_line("sofa", 30, 20)
     assert batch.can_allocate(line) == True
-    pytest.fail("todo")
 
 
 def test_cannot_allocate_if_available_smaller_than_required():
     batch, line = make_batch_and_order_line("sofa", 10, 20)
     assert batch.can_allocate(line) == False
-    pytest.fail("todo")
 
 
 def test_can_allocate_if_available_equal_to_required():
     batch, line = make_batch_and_order_line("sofa", 20, 20)
     assert batch.can_allocate(line) == True
-    pytest.fail("todo")
 
 
 def test_prefers_warehouse_batches_to_shipments():
@@ -50,8 +46,7 @@ def test_prefers_warehouse_batches_to_shipments():
     shipment_batch = Batch("batch-s", "sofa", 20, eta=later)
     line = OrderLine("line-1", "sofa", 8)
     allocate(line, [warehouse_batch, shipment_batch])
-    assert warehouse.available_qty == 12 and shipment_batch.available_qty == 20
-    pytest.fail("todo")
+    assert warehouse_batch.available_qty == 12 and shipment_batch.available_qty == 20
 
 
 def test_prefers_earlier_batches():
@@ -60,7 +55,24 @@ def test_prefers_earlier_batches():
     late_batch = Batch("batch-w", "sofa", 20, eta=today+timedelta(days=10))
     line = OrderLine("line-1", "sofa", 8)
     allocate(line, [early_batch, today_batch, late_batch])
-    assert early_batch.availabe_qty == 12
+    assert early_batch.available_qty == 12
     assert today_batch.available_qty == 20
     assert late_batch.available_qty == 20
-    pytest.fail("todo")
+
+def test_deallocate_batch():
+    batch, line = make_batch_and_order_line("sofa", 30, 20)
+    batch.allocate(line)
+    print(batch._allocations)
+    batch.deallocate(line)
+    assert batch.available_qty == 30
+    
+def test_allocate_more_than_avl():
+    batch, line = make_batch_and_order_line("sofa", 30, 40)
+    batch.allocate(line)
+    assert batch.available_qty == 30
+    
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_order_line("ANGULAR-DESK", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_qty == 18
